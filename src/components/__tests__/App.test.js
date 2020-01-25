c54e0c8ce2bfcfc0f7967a919d686d1106990d38
Julio <pastor@indava.com>
2020-01-25 00:45:49 -0600

    Se agrega test sencillo de prueba

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,7 @@
+import { React } from "react";
+import { mount } from "enzyme";
+
+test("hellow world", () => {
+  const wrapper = mount(<p>Hello Jest!</p>);
+  expect(wrapper.text()).toMatch("Hello jest!");
+});
