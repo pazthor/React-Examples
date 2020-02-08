c1d035bcc8dc06c218ce720ee98344650f039c55
Julio <pastor@indava.com>
2020-02-07 23:47:14 -0600

    Se agrega lista de ingredientes a la pizza

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Pizza = ({ingredients}) => {
-  
+const Pizza = ({ ingredients }) => {
   return (
-    <h1>Pizza</h1>
-  )
-  
-}
+    <>
+      <h1>Pizza</h1>
+      <ul>
+        {ingredients.map(ingredient => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Pizza;
