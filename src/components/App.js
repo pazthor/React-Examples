aa871806be9f3183febd939486a5f491b755c11d
Julio <pastor@indava.com>
2020-02-07 22:51:36 -0600

    Se actualiza app

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,12 @@
-import React from 'react';
-import unsplash from '../api/unsplash'
-import SearchBar from './SearchBar';
-import ImagesList from './ImagesList';
-import {Title, Wrapper, Buttons, TomatoButton} from './Title'
+import React from "react";
 
-class App extends React.Component{
-  state = { images: []};
-
-  onSearchSubmit = async term => {
-    const response = await unsplash.get('/search/photos', {
-      params: {query: term}
-    });
-    
-    this.setState({images: response.data.results});
-  }
-
-  render(){
-    return (
-      <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
-        <ImagesList images={this.state.images} />
-        
-      <Wrapper>
-      <Title>
-        Hello World!
-      </Title>
-  </Wrapper>
-  <div>
-    <Buttons >
-      Normal Buttons
-    </Buttons>
-    <TomatoButton>Tomato Buttons</TomatoButton>
-  </div>
-
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h2>PlayGround</h2>
+      <h2> Esto es una aplicacion de prueba con fines didacticos</h2>
+    </div>
+  );
+};
 
 export default App;
